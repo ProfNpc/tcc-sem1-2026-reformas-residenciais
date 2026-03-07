34842a3c3fa4d18aef9c43707a63732c1d2fa99d
Jaciel <rm101673@estudante.fieb.edu.br>
2026-03-06 21:51:25 -0300

    Versao Inicial

---
+++ Back/refores/src/main/java/br/com/belval/refores/ReforesApplication.java
@@ -0,0 +1,13 @@
+package br.com.belval.refores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReforesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReforesApplication.class, args);
+	}
+
+}
---
+++ Back/refores/src/test/java/br/com/belval/refores/ReforesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.belval.refores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReforesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
