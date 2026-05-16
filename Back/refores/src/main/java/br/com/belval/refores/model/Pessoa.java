abdd9f890441dcf49dd97b9657a791223783f74e
Nicollas <rm101838@estudante.fieb.edu.br>
2026-05-15 22:54:46 -0300

    Pessoas(Nicollas)

---
+++ Back/refores/src/main/java/br/com/belval/refores/model/Pessoa.java
@@ -0,0 +1,104 @@
+package br.com.belval.refores.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_Pessoa")
+public class Pessoa {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "cpf", nullable = false, length = 11)
+	private String cpf;
+	
+	@Column(name = "nome", nullable = false, length = 100)
+	private String nome;
+	
+	@Column(name = "telefone", nullable = false, length = 15)
+	private String telefone;
+	
+	@Column(name = "email", nullable = false, unique = true, length = 100)
+	private String email;
+	
+ public Pessoa() {
+		
+	}
+	
+	public Integer getId() {
+		return this.id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, email, id, nome, telefone);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(telefone, other.telefone);
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [id=" + id + ", cpf=" + cpf + ", nome=" + nome + ", telefone=" + telefone + ", email=" + email
+				+ "]";
+	}
+
+	
+	
+	
+}
---
+++ Back/refores/src/main/java/br/com/belval/refores/repository/PessoaRepository.java
@@ -0,0 +1,15 @@
+package br.com.belval.refores.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.belval.refores.model.Pessoa;
+
+
+
+@Repository
+public interface PessoaRepository extends CrudRepository<Pessoa, Integer>{
+	
+}
