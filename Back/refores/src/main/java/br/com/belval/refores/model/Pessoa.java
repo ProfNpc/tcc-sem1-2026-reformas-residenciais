73a74c8ee57a05940d8f8f1dde030fae5e378f2d
Elisa <rm101578@estudante.fieb.edu.br>
2026-05-20 20:58:54 -0300

    Add pessoa

---
+++ Back/refores/src/main/java/br/com/belval/refores/model/Pessoa.java
@@ -0,0 +1,117 @@
+package br.com.belval.refores.model;
+
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * Corresponde à tabela pessoa
+ */
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "nome", nullable = false, length = 150)
+	private String nome;
+	
+	@Column(name = "cpf", nullable = false, unique = true, length = 14)
+	private String cpf;
+	
+	@Column(name = "telefone", length = 20)
+	private String telefone;
+	
+	@Column(name = "endereco", length = 255)
+	private String endereco;
+	
+	@Column(name = "email", length = 100)
+	private String email;
+	
+	
+	public Pessoa() {
+		super();
+	}
+	
+	
+	public Integer getId() {
+		return this.id;
+	}
+	
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Pessoa [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", telefone=" + telefone + ", endereco="
+				+ endereco + ", email=" + email + "]";
+	}
+}
