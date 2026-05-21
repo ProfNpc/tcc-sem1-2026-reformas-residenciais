83979196cad50ffe13398e074a51940ad8217af3
Elisa <rm101578@estudante.fieb.edu.br>
2026-05-20 21:13:19 -0300

    Merge2

--- Back/refores/src/main/java/br/com/belval/refores/model/Pessoa.java
+++ Back/refores/src/main/java/br/com/belval/refores/model/Pessoa.java
@@ -1,9 +1,8 @@
 package br.com.belval.refores.model;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
+
+
 import java.util.Objects;
 
 import jakarta.persistence.Column;
@@ -13,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-<<<<<<< HEAD
 /**
  * Corresponde à tabela pessoa
  */
@@ -46,46 +44,17 @@ public class Pessoa {
 	}
 	
 	
-=======
-
-@Entity
-@Table(name = "tb_Pessoa")
-public class Pessoa {
-	
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	@Column(name = "cpf", nullable = false, length = 11)
-	private String cpf;
-	
-	@Column(name = "nome", nullable = false, length = 100)
-	private String nome;
-	
-	@Column(name = "telefone", nullable = false, length = 15)
-	private String telefone;
-	
-	@Column(name = "email", nullable = false, unique = true, length = 100)
-	private String email;
-	
- public Pessoa() {
-		
-	}
-	
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
 	public Integer getId() {
 		return this.id;
 	}
 	
-<<<<<<< HEAD
+
 	
-=======
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-<<<<<<< HEAD
 	public String getNome() {
 		return nome;
 	}
@@ -94,8 +63,6 @@ public class Pessoa {
 		this.nome = nome;
 	}
 
-=======
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
 	public String getCpf() {
 		return cpf;
 	}
@@ -104,17 +71,8 @@ public class Pessoa {
 		this.cpf = cpf;
 	}
 
-<<<<<<< HEAD
-=======
-	public String getNome() {
-		return nome;
-	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
 	public String getTelefone() {
 		return telefone;
 	}
@@ -123,7 +81,6 @@ public class Pessoa {
 		this.telefone = telefone;
 	}
 
-<<<<<<< HEAD
 	public String getEndereco() {
 		return endereco;
 	}
@@ -132,8 +89,6 @@ public class Pessoa {
 		this.endereco = endereco;
 	}
 
-=======
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
 	public String getEmail() {
 		return email;
 	}
@@ -144,11 +99,7 @@ public class Pessoa {
 
 	@Override
 	public int hashCode() {
-<<<<<<< HEAD
 		return Objects.hash(id);
-=======
-		return Objects.hash(cpf, email, id, nome, telefone);
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
 	}
 
 	@Override
@@ -160,7 +111,6 @@ public class Pessoa {
 		if (getClass() != obj.getClass())
 			return false;
 		Pessoa other = (Pessoa) obj;
-<<<<<<< HEAD
 		return Objects.equals(id, other.id);
 	}
 
@@ -170,19 +120,5 @@ public class Pessoa {
 		return "Pessoa [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", telefone=" + telefone + ", endereco="
 				+ endereco + ", email=" + email + "]";
 	}
-=======
-		return Objects.equals(cpf, other.cpf) && Objects.equals(email, other.email) && Objects.equals(id, other.id)
-				&& Objects.equals(nome, other.nome) && Objects.equals(telefone, other.telefone);
-	}
 
-	@Override
-	public String toString() {
-		return "Pessoa [id=" + id + ", cpf=" + cpf + ", nome=" + nome + ", telefone=" + telefone + ", email=" + email
-				+ "]";
-	}
-
-	
-	
-	
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
 }
--- Back/refores/src/main/java/br/com/belval/refores/repository/PessoaRepository.java
+++ Back/refores/src/main/java/br/com/belval/refores/repository/PessoaRepository.java
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
+
 
 package br.com.belval.refores.repository;
  
-=======
-package br.com.belval.refores.repository;
 
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.belval.refores.model.Pessoa;
-<<<<<<< HEAD
  
 @Repository
 public interface PessoaRepository extends CrudRepository<Pessoa, Integer> {
@@ -22,17 +18,3 @@ public interface PessoaRepository extends CrudRepository<Pessoa, Integer> {
 
 }
 
-
-
-
-
-
-=======
-
-
-
-@Repository
-public interface PessoaRepository extends CrudRepository<Pessoa, Integer>{
-	
-}
->>>>>>> 28ffbee615de0532721fc95725e7237e1f9e6e75
