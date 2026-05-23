62afbf61bf8d2a4bbc690ce1b801afabcb10eef0
FernandaCosta <rm101978@estudante.fieb.edu.br>
2026-05-22 22:42:26 -0300

    Add conf CORS

--- Back/refores/src/test/java/br/com/belval/refores/CorsConfig.java
+++ Back/refores/src/test/java/br/com/belval/refores/CorsConfig.java
@@ -1,5 +1,32 @@
+
+
 package br.com.belval.refores;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * PARA FAZER A LIGAÇÃO DO BACK END COM O FRONT END.
+ */
+
+/**
+ * CORS=Cross origin resource sharing/
+ * Compartilhamento cruzado de origem d recurso
+ * 
+ *  */
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+	@Override
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:5173")
+		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH" )
+		.allowedHeaders("*");
+	
 }
+	
+}
+
